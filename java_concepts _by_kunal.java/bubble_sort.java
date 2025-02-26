d04b8c053ec3b13439a0697c2795a125a66506c8
Paaapi <paravdeepwalia@gmail.com>
2025-02-26 10:20:55 +0530

    commit

--- java_concepts _by_kunal.java/bubble_sort.java
+++ java_concepts _by_kunal.java/bubble_sort.java
@@ -7,13 +7,19 @@ public class bubble_sort {
         System.out.println(Arrays.toString(arr));
     }
     static void sort(int[] arr){
+        boolean swapp;
         for(int i=0;i<arr.length;i++){
+            swapp= false;
             for(int j=1;j<arr.length-i;j++){
                 if(arr[j]<arr[j-1]){
                     int temp=arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=temp;
+                    swapp=true;
                 }
+            }// if you didn't swapped for a particular value of i, it sayys array is sorted
+            if(!swapp){ // !false=true
+                break;
             }
         }
     }
